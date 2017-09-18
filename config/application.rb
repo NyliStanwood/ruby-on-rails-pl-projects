f629acfec20a5177a1976d06867298d1f4e4e595
Marilyn Stanwood <nyliz@live.com>
2017-09-17 23:14:26 -0400

    Added authentication to RSpotify of the application

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module MusicAppNyli1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # config/application.rb
+    RSpotify::authenticate(ENV['spotify_client_id'] , ENV['spotify_client_secret'])
+
+    puts "module MusicApp class Application values:"
+    puts ENV['spotify_client_id']
+    secret = ENV['spotify_client_secret']
+    puts secret
   end
 end
